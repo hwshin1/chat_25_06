5c3196270febc81faa334a4894e04b847f277c94
hwshin1 <hwshin5044@gmail.com>
2025-06-12 15:19:08 +0900

    메세지 작성시 작성자, 내용을 JSON 형태로 전달

--- src/main/java/com/koreait/exam/chat_25_06/controller/ChatController.java
+++ src/main/java/com/koreait/exam/chat_25_06/controller/ChatController.java
@@ -14,14 +14,16 @@ public class ChatController {
 
     List<ChatMessage> chatMessages =  new ArrayList<>();
 
-    public record writeMessageResponse(long id) { }
+    public record writeChatMessageResponse(long id, String authorName, String content) {}
+
+    public record writeChatMessageRequest(String authorName, String content) {}
 
     @PostMapping("/writeMessage")
     @ResponseBody
-    public RsData<writeMessageResponse> writeMessage() {
-        ChatMessage chatMessage = new ChatMessage("홍길동", "안녕");
+    public RsData<writeChatMessageResponse> writeMessage(@RequestBody writeChatMessageRequest req) {
+        ChatMessage chatMessage = new ChatMessage(req.authorName, req.content);
         chatMessages.add(chatMessage);
-        return new RsData<>("S_1", "메세지 작성", new writeMessageResponse(chatMessage.getId()));
+        return new RsData<>("S_1", "메세지 작성", new writeChatMessageResponse(chatMessage.getId(), chatMessage.getAuthorName(), chatMessage.getContent()));
     }
 
     @GetMapping("/messages")
