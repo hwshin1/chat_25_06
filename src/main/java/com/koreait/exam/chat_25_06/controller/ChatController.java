d8832cc47b0917b3006af58ebd09f4faf493ce46
hwshin1 <hwshin5044@gmail.com>
2025-06-12 16:01:43 +0900

    messagesRequest filter

--- src/main/java/com/koreait/exam/chat_25_06/controller/ChatController.java
+++ src/main/java/com/koreait/exam/chat_25_06/controller/ChatController.java
@@ -2,14 +2,17 @@ package com.koreait.exam.chat_25_06.controller;
 
 import com.koreait.exam.chat_25_06.ChatMessage;
 import com.koreait.exam.chat_25_06.RsData;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/chat")
+@Slf4j
 public class ChatController {
 
     List<ChatMessage> chatMessages =  new ArrayList<>();
@@ -28,10 +31,31 @@ public class ChatController {
 
     public record messagesResponse(List<ChatMessage> chatMessages, long count) {}
 
+    //fromId => 만약 채팅창이 있다고 할 때 기존 채팅창에서 대화내용을 불러오려는 번호
+    public record messagesRequest(Long fromId) {}
+
     @GetMapping("/messages")
     @ResponseBody
-    public RsData<messagesResponse> showMessages() {
+    public RsData<messagesResponse> showMessages(messagesRequest req) {
+
+        List<ChatMessage> messages = chatMessages;
+
+        log.debug("req: {}", req);
+
+        if (req.fromId != null) {
+            // 해당 번호의 채팅 메세지가 전체 리스트의 몇번째 인덱스인지?
+            int index = IntStream.range(0, messages.size())
+                    .filter(i -> chatMessages.get(i).getId() == req.fromId)
+                    .findFirst()
+                    .orElse(-1);
+
+            if (index != -1) {
+                // 만약에 index가 -1 이 아니라면? 0번부터 index번 까지 제거한 리스트를 만든다.
+                messages = messages.subList(index + 1, messages.size());
+            }
+        }
+
         return new RsData<>("S_1", "성공",
-                new messagesResponse(chatMessages, chatMessages.size()));
+                new messagesResponse(messages, messages.size()));
     }
 }
